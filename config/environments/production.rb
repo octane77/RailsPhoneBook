68c4698f68be83d11dc9f10f41db6315db3a52d4
Emmanuel Oluwadurotimi Upah <octobereagle1@gmail.com>
2022-10-17 14:33:53 +0100

    Developing RailsPhonebook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,6 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
