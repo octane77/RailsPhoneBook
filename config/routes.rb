27f49ecacb7ecea0fbfd0bfc01c30b0272722370
Emmanuel Oluwadurotimi Upah <octobereagle1@gmail.com>
2022-10-18 17:33:36 +0100

    Developing RailsPhonebook

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/home/about'
   get '/contacts/new'
   root 'contacts#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
